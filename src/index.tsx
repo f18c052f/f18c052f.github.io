f39831f30baa9f8c6c9cf44144c802165abda2e9
f18c052f@gmail.com <f18c052f@gmail.com>
2023-09-24 13:59:54 +0900

    フォント設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/open-sans';
+import '@fontsource/raleway';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
     <App />
     </ChakraProvider>
   </React.StrictMode>
