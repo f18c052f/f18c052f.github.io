4f20c1949e368b35c9706c025849781ac0bdb7ad
f18c052f@gmail.com <f18c052f@gmail.com>
2023-09-28 03:16:03 +0900

    スマホ上見た目微調整

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -11,51 +11,51 @@ import Hobbies from './Hobbies';
 
 const Contents: React.FC = () => {
     return (
-        <SimpleGrid minChildWidth='400px' spacing='10px' padding="20px">
+        <SimpleGrid minChildWidth='360px' spacing='10px' padding="20px">
             <Box boxShadow="md" borderRadius='md' height='400px' p='6'>
-                <Text fontSize='4xl'>
+                <Text fontSize='3xl'>
                     Work Experience 🧙🏼‍♂️
                 </Text>
                 <Box height='15px' />
                 <Experiences />              
             </Box>
             <Box boxShadow="md" borderRadius='md' height='400px' p='6'>
-                <Text fontSize='4xl'>
-                        Education  📖
+                <Text fontSize='3xl'>
+                        Education 📖
                 </Text>
                 <Box height='15px' />
                 <Education />
             </Box>
             <Box boxShadow="md" borderRadius='md' height='720px' p='6'>
-                <Text fontSize='4xl'>
+                <Text fontSize='3xl'>
                         Archievements 💎
                 </Text>
                 <Box height='15px' />
                 <Achievement />
             </Box>
             <Box boxShadow="md" borderRadius='md' height='650px' p='6'>
-                <Text fontSize='4xl'>
+                <Text fontSize='3xl'>
                         Certifications 📑
                 </Text>
                 <Box height='15px' />
                 <Certifications />
             </Box>
             <Box boxShadow="md" borderRadius='md' height='250px' p='6'>
-                <Text fontSize='4xl'>
-                        Programming Skills 💻
+                <Text fontSize='3xl'>
+                        Skills 💻
                 </Text>
                 <Box height='15px' />
                 <Programming />
             </Box>
             <Box boxShadow="md" borderRadius='md' height='500px' p='6'>
-                <Text fontSize='4xl'>
+                <Text fontSize='3xl'>
                         Fields of interest 🔍
                 </Text>
                 <Box height='15px' />
                 <Fields />
             </Box>
             <Box boxShadow="md" borderRadius='md' height='900px' p='6'>
-                <Text fontSize='4xl'>
+                <Text fontSize='3xl'>
                         Hobbies 🏸
                 </Text>
                 <Box height='15px' />
