4585e3c8bb01255ce9004462f3707a587b80a34c
f18c052f@gmail.com <f18c052f@gmail.com>
2023-09-27 23:04:36 +0900

    Contactコンテンツ作成

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,25 @@
+import { Center, Text, Box, VStack, HStack } from '@chakra-ui/react';
+import { EmailIcon } from '@chakra-ui/icons';
 import React from 'react';
 
 const Contact: React.FC = () => {
     return (
-        <></>
+        <Box padding='50px'>
+            <Center >
+                <VStack>
+                    <Text fontSize='xl'>
+                        Please contact me :)
+                    </Text>
+                    <HStack>
+                        <EmailIcon />
+                        <Text>
+                            yuta.ohara1995@gmail.com
+                        </Text>
+                    </HStack>
+                </VStack>
+            </Center>
+        </Box>
+
     );
 }
 
